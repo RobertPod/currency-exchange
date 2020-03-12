d14e948ba4418c02696536fff6121e7a7d5cb8b7
Robert Podsiadly <rob.podsiadly@gmail.com>
2020-03-12 22:20:56 +0100

    Add method getExchangeRateListInEUR to RatesProvider

--- src/main/java/com/exchange/app/RatesProvider.java
+++ src/main/java/com/exchange/app/RatesProvider.java
@@ -1,6 +1,14 @@
 package com.exchange.app;
 
+import java.util.ArrayList;
 import java.util.Currency;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.joda.time.DateTime;
 
 public class RatesProvider {
     private ForeignExchangeRatesApiClient apiClient;
@@ -20,5 +28,23 @@ public class RatesProvider {
 
     public Double getExchangeRate(Currency requested, Currency exchanged) {
         return apiClient.getLatestRates(exchanged.getCurrencyCode()).get(requested.getCurrencyCode());
+    }
+
+    public Map<DateTime, Double> getExchangeRateListInEUR(Currency requested, DateTime start_at,
+        DateTime end_at) {
+        List<ExchangeRates> exchangeRateses;
+
+        try {
+            exchangeRateses = apiClient.getHistoricalRates(start_at, end_at);
+        } catch (IllegalArgumentException e) {
+            throw new CurrencyNotSupportedException(
+                "Currency " + requested.getCurrencyCode()
+                    + " is not supported, or or incorrect date range");
+        }
+
+        return exchangeRateses.stream().collect(Collectors
+            .toMap(ExchangeRates::getDate,
+                exchangeRates -> exchangeRates.get(requested.getCurrencyCode()), (a, b) -> b,
+                TreeMap::new));
     }
 }
